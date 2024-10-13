0f57e3c1b3a2bc7f20b179172b3cf62735e167dc
DiegoGp227 <DiegoGp227@gmail.com>
2024-10-13 14:21:35 -0500

    Revert "help"
    This reverts commit 94e07534d4cf94ae36b19eb66d4b4b7875a8b246.

--- Backend/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-// import { pool } from "../db/db.js";
-
-
-export const ping = async (req, res) => { 
-    const [result] = await pool.query ("SELECT 'pong'");
-    res.json(result[0])
-}
--- Backend/routes/activitys.routes.js
+++ Backend/routes/activitys.routes.js
@@ -1,19 +1,4 @@
-import {Router} from "express";
-import {getActivity} from "../controllers/activitys.controllers.js";
-import {createActivity} from "../controllers/activitys.controllers.js";
-import {updateActivity} from "../controllers/activitys.controllers.js";
-import {deleteActivity} from "../controllers/activitys.controllers.js";
-
+import {Router, Router} from "express";
 
 const router = Router()
-
-router.get("/activity", getActivity)
-
-router.post("/activity", createActivity)
-
-router.put("/activity", updateActivity)
-
-router.delete("/activity", deleteActivity)
-
-export default router
 
